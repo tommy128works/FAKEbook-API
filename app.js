4ab3c8c5157efa2594aaa3f9d76fc46391ab59f1
tommy128works <tommy128works@gmail.com>
2023-12-04 14:23:37 -0500

    prog: create file structure and initial code
    Initial code made for

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-
-var app = express();
-
 require("dotenv").config();
-// for testing
-// console.log(process.env.SECRET_KEY);
+
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+const mongoDb = process.env.MONGODB_URI;
+mongoose.connect(mongoDb);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Register new user
+
+
 module.exports = router;
